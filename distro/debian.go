6f6f9ad3cce37a621cb30daec184df9463b4ca4b
alihassan4198-tech <alihassan4198@gmail.com>
2022-06-29 02:14:35 -0700

    Debian Software Installed

--- distro/debian.go
+++ distro/debian.go
@@ -1,7 +1,10 @@
 package distro
 
 import (
+	"fmt"
 	"machine_info_gatherer/model"
+	"os/exec"
+	"strings"
 )
 
 // Debian
@@ -10,8 +13,52 @@ type DebianBased struct {
 	LinuxBase
 }
 
+const (
+	find0 = "Name"
+	find1 = "Version"
+	find2 = "Architecture"
+	find3 = "Description"
+)
+
 func (lb *DebianBased) GetComputerSoftwaresInstalled() (*model.ComputerSoftwaresInstalled, error) {
 	comSoftInst := model.ComputerSoftwaresInstalled{}
+
+	installSoft, err := exec.Command("dpkg", "-l").Output()
+	if err != nil {
+		fmt.Println(err)
+		return &comSoftInst, err
+	}
+	splittedInstallSoft := strings.Split(string(installSoft), "\n")
+
+	fmt.Println("before", len(splittedInstallSoft))
+
+	for i, soft := range splittedInstallSoft {
+		if strings.Contains(soft, find0) &&
+			strings.Contains(soft, find1) &&
+			strings.Contains(soft, find2) &&
+			strings.Contains(soft, find3) {
+			// Remove first 2 useless record strings
+			splittedInstallSoft = splittedInstallSoft[i+2:]
+			break
+		}
+	}
+
+	// Remove last \n record string
+	splittedInstallSoft = splittedInstallSoft[:len(splittedInstallSoft)-1]
+
+	// Convert to Strcut
+	for i, soft := range splittedInstallSoft {
+		soft = strings.Join(strings.Fields(soft), " ")
+		splittedSoft := strings.Split(soft, " ")
+		softInstall := model.SoftwareInstalled{}
+		fmt.Println(i, splittedSoft)
+		softInstall.Display_name = splittedSoft[1]
+		softInstall.Version = splittedSoft[2]
+
+		comSoftInst.SoftwaresInstalled = append(comSoftInst.SoftwaresInstalled, softInstall)
+	}
+
+	comSoftInst.Total_software = len(comSoftInst.SoftwaresInstalled)
 
 	return &comSoftInst, nil
 }
