1bbd2d2ba640309855a1ace0349d2f8a26b69676
alihassan4198-tec <alihassan4198@gmail.com>
2022-08-30 07:12:31 -0700

    update

--- cronJob.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo crontab -l > cron_bkp
-cp ./machine_info_gatherer /usr/bin
-# sudo crontab cron_bkp
-# sudo rm cron_bkp
--- serviceInstall.sh
+++ serviceInstall.sh
@@ -1,12 +1,11 @@
 echo "Deleting"
 systemctl stop machine_info_service.service
-#rm /usr/bin/linux_machine_info_gatherer
-rm /usr/bin/machine_info_gatherer
 rm /lib/systemd/system/machine_info_service.service
 
+
 echo "Coping"
-cp ./machine_info_gatherer /usr/bin
 cp ./machine_info_service.service /lib/systemd/system/
+
 
 echo "Starting"
 systemctl enable machine_info_service.service
