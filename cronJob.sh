218283ad3c9fe630386e6408b7cd420d5f7795c2
alihassan4198-tec <alihassan4198@gmail.com>
2022-09-06 07:28:51 -0700

    update

--- cronJob.sh
+++ cronJob.sh
@@ -1,26 +1,35 @@
 
 if [ -d /tmp/machineinfocsv ]
 then 
-    echo "Directory Already Exists Can't Create Again"
+    echo "Directory Already Exists, Can't Create Again"
 else
+    echo "Directory Creating..."
     mkdir /tmp/machineinfocsv
     chmod 777 /tmp/machineinfocsv #ADD FOR PERMISSIONS
+    # touch /tmp/machineinfocsv/file.txt
+    # chmod 777 /tmp/machineinfocsv/file.txt #ADD FOR PERMISSIONS
+    
 fi
 
 if [ -e /tmp/cron.txt ]
 then 
-    echo "Cron Already Installed"
+    echo "Cron Already Installed, Now Overriding"
+    # machine-info-selector/MachineInfoReader "/tmp/machineinfocsv"
+    # cd /tmp/machineinfocsv
     crontab -l | grep -v '/MachineInfoReader/machine_info_gatherer'  | crontab -
-    crontab -l > mycron
-    echo "* * * * *" /MachineInfoReader/machine_info_gatherer >> mycron
-    crontab mycron
-    rm mycron
+    * * * * * /MachineInfoReader/machine_info_gatherer >> /tmp/machineinfocsv
+ 
+    # * * * * * /MachineInfoReader/machine_info_gatherer >> /tmp/machineinfocsv/file.txt
+    # crontab -l > mycron
+    # echo "* * * * *" /MachineInfoReader/machine_info_gatherer  >> mycron
+    # crontab mycron
+    # rm mycron
     
 else
-    echo "Cron Not Installed"
+    echo "Cron Not Installed, Installing"
     touch /tmp/cron.txt
     crontab -l > mycron
-    echo "* 18 * * *" /MachineInfoReader/machine_info_gatherer >> mycron
+    echo "* * * * *" /MachineInfoReader/machine_info_gatherer  >> mycron
     crontab mycron
     rm mycron
 fi
