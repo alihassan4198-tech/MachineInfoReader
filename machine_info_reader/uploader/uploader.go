e123655022c1d0dea7590a050f26f34c38c32613
ali hassan <alihassan4198@gmail.com>
2024-07-07 03:42:39 +0500

    upload apis with go routine
    go routines and waitgroup is added for api calling side by side !

--- machine_info_reader/uploader/uploader.go
+++ machine_info_reader/uploader/uploader.go
@@ -4,21 +4,26 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"net/http"
-
+	"log"
 	"machine_info_gatherer/model"
+	"net/http"
 	"strings"
+	"sync"
+)
+
+var (
+	client  = &http.Client{}
+	baseURL = "http://localhost:3020/api/v1/"
 )
 
 func httpRequest(data interface{}, path string) error {
-
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return fmt.Errorf("error marshalling JSON: %v", err)
 	}
 
-	url := fmt.Sprintf("http://localhost:3020/%s", path)
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	url := baseURL + path
+	resp, err := client.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		return fmt.Errorf("error making POST request: %v", err)
 	}
@@ -28,18 +33,62 @@ func httpRequest(data interface{}, path string) error {
 		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
-	fmt.Println("Data uploaded successfully")
+	log.Println("Data uploaded successfully to", url)
 	return nil
 }
 
 func UploadJsonFile(info *model.ComputerInfoType, apiPath string) {
 
-	prefix := "api/v1/"
-
-	if strings.Contains(apiPath, "baseboard") {
-		err := httpRequest(info.ComputerBaseboard, prefix+apiPath)
-		if err != nil {
-			fmt.Printf("Error uploading JSON data: %v\n", err)
+	var wg sync.WaitGroup
+	checkAndUpload := func(data interface{}, path string) {
+		defer wg.Done()
+		if err := httpRequest(data, path); err != nil {
+			log.Printf("Error uploading JSON data to %s: %v\n", path, err)
 		}
 	}
+	if strings.Contains(apiPath, "baseboard") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerBaseboard, apiPath)
+	}
+	if strings.Contains(apiPath, "bios") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerBios, apiPath)
+	}
+	if strings.Contains(apiPath, "cpu") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerCPU.CPUCores, apiPath)
+	}
+	if strings.Contains(apiPath, "endpoint-protection") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerEndpointProtection, apiPath)
+	}
+	if strings.Contains(apiPath, "fw-rules") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerFirewallRules.FW_rules, apiPath)
+	}
+	if strings.Contains(apiPath, "nics") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerNICS, apiPath)
+	}
+	if strings.Contains(apiPath, "os") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerOS, apiPath)
+	}
+	if strings.Contains(apiPath, "patches") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerPatches.Patches, apiPath)
+	}
+	if strings.Contains(apiPath, "services") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerServices, apiPath)
+	}
+	if strings.Contains(apiPath, "software") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerSoftwaresInstalled, apiPath)
+	}
+	if strings.Contains(apiPath, "system") {
+		wg.Add(1)
+		go checkAndUpload(info.ComputerSystem, apiPath)
+	}
+	wg.Wait()
 }
