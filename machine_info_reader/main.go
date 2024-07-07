9860d15d14890454f8f124d09b1a33379040eb7e
ali hassan <alihassan4198@gmail.com>
2024-07-07 15:08:00 +0500

    update

--- machine_info_reader/main.go
+++ machine_info_reader/main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
-	"machine_info_gatherer/common"
 	"machine_info_gatherer/debug"
 	"machine_info_gatherer/gatherer"
 	"machine_info_gatherer/uploader"
-	"os"
 )
 
 func main() {
 	fmt.Println("Launching Machine Info Gatherer...")
-
-	// common.SetSudoPassword("123456")
-	common.PathSetter(os.Args[1])
-	if debug.Debug() {
-		fmt.Println("common.PathSetter(os.Args[1])")
-	}
 	i := gatherer.GetInstance()
 	if debug.Debug() {
 		fmt.Println("i := gatherer.GetInstance()")
